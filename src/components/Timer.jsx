b5c9d78a6f823fc4333598ecbf94803991c6e3e7
Josh <joshcotton93@gmail.com>
2025-07-22 16:17:58 -0500

    cleaned up components and added local storage

---
+++ src/components/Timer.jsx
@@ -0,0 +1,48 @@
+import { useState, useRef } from "react";
+import TimerDisplay from "./TimerDisplay";
+import TimerControls from "./TimerControls";
+import { useEffect } from "react";
+const Timer = () => {
+  const timerRef = useRef(null);
+  const [time, setTime] = useState(() => {
+    return Number(localStorage.getItem("time") || 0);
+  });
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("time", time);
+  }, [time]);
+  const toggleTimer = () => {
+    if (isRunning) {
+      //clear the interval to stop timer
+      clearInterval(timerRef.current);
+      timerRef.current = null;
+    } else {
+      // start timer
+      timerRef.current = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    }
+    setIsRunning(!isRunning);
+  };
+
+  const resetTimer = () => {
+    clearInterval(timerRef.current);
+    setIsRunning(false);
+    setTime(0);
+    timerRef.current(null);
+    localStorage.removeItem("time");
+  };
+  return (
+    <div>
+      <TimerDisplay time={time} />
+      <TimerControls
+        isRunning={isRunning}
+        onToggle={toggleTimer}
+        onReset={resetTimer}
+      />
+    </div>
+  );
+};
+
+export default Timer;
